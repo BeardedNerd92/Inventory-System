30a37d6d24539d0085490cdea64a3da69569f0d9
BeardedNerd92 <IntMain92@gmail.com>
2026-01-23 14:51:42 -0600

    I wrote the render list items fucntion to render inventory items to the UI.

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
+
 const productName = document.getElementById("product-name");
 const quantity = document.getElementById("quantity");
 const form = document.getElementById("inventory-form");
+const inventoryList = document.getElementById("inventory-list")
 
+form.addEventListener("submit", handleFormData);
+const inventory = [];
 
 
 function handleFormData(e) {
     e.preventDefault();
-    const productNameData = productName.value;
+    const productNameData = productName.value.trim();
     const quantityData = Number(quantity.value); 
-    if (productNameData.trim() === "") {
+    if (productNameData === "") {
         return;
     }
     if (quantityData < 0 || Number.isNaN(quantityData)) {
         return;
     }
+    inventory.push({
+        name: productNameData,
+        quantity: quantityData
+    });
+    renderListItems();
+    deleteListItem();
+    productName.value = "";
+    quantity.value = "";
+    productName.focus();
 }
-form.addEventListener("submit", handleFormData);
+
+function renderListItems() {
+     inventoryList.innerHTML = "";
+     inventory.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = `${item.name} - ${item.quantity}`
+        inventoryList.appendChild(li);
+     })
+}
+
+function deleteListItem() {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete Item"
+    deleteBtn.addEventListener("click", () => {
+        inventory.value = "";
+    })
+    inventoryList.appendChild(deleteBtn);
+}
